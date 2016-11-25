a0b84bbda1db6d39f37a759ecc1fde4c8447c0f1
Rui <rui@Students-MacBook-Pro.local>
2016-11-24 21:00:30 -0500

    add settings when application load

--- Map-C/ViewController.m
+++ Map-C/ViewController.m
@@ -17,6 +17,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //initialize locationManager
+    CLLocationManager *locationManager=[[CLLocationManager alloc]init];
+    
+    self.locationManager=locationManager;
+    
+    //location Services Enabled
+    if (![CLLocationManager locationServicesEnabled]) {
+        NSLog(@"location Services is not enabled");
+    }
+    
+    //Ask permission
+    if ([UIDevice currentDevice].systemVersion.floatValue >=8.0) {
+        //always Authorization
+        [self.locationManager requestAlwaysAuthorization];
+        //use Authorization
+        //[self.locationManager requestWhenInUseAuthorization];
+    }
+    
+    /*
+     MKUserTrackingModeNone  //The map does not follow the user location.
+     MKUserTrackingModeFollow  //The map follows the user location
+     MKUserTrackingModeFollowWithHeading  //The map follows the user location and rotates when the heading changes.
+     */
+    
+    // set tracking mode
+    self.mapView.userTrackingMode=MKUserTrackingModeFollow;
+    
+    /*
+     MKMapTypeStandard
+     MKMapTypeSatellite
+     MKMapTypeHybrid
+     MKMapTypeSatelliteFlyover
+     MKMapTypeHybridFlyover
+     */
+    
+    // show traffic
+    self.mapView.showsTraffic=NO;
+    
+    //delegate
+    self.mapView.delegate=self;
+    
 }
 
 
