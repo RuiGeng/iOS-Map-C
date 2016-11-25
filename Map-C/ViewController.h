eeedf6ea2599fde4a3991826eef2a0628ebb713b
Rui <rui@Students-MacBook-Pro.local>
2016-11-24 20:46:06 -0500

    add a mapkit to project

--- Map-C/ViewController.h
+++ Map-C/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (nonatomic,strong)CLLocationManager *locationManager;
 
 @end
 
