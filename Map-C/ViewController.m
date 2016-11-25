15f25ec2f63fdfc01b12d34a474674796f481c70
Rui <rui@Students-MacBook-Pro.local>
2016-11-24 21:27:19 -0500

    add some functions

--- Map-C/ViewController.m
+++ Map-C/ViewController.m
@@ -62,6 +62,76 @@
 }
 
 
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+    //Create CLGeocoder Object
+    CLGeocoder *geocoder=[[CLGeocoder alloc]init];
+    //reverse user location
+    [geocoder reverseGeocodeLocation:userLocation.location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (error!=nil || placemarks.count==0) {
+            return ;
+        }
+        //get placemark
+        CLPlacemark *placemark=[placemarks firstObject];
+        //Set user location titile
+        userLocation.title=placemark.locality;
+        //Set user location subtitile
+        userLocation.subtitle=placemark.name;
+    }];
+    
+    /*
+     //geographical coordinate structure
+    CLLocationCoordinate2D loc = [userLocation coordinate];
+    
+    NSString longitude = [NSString stringWithFormat:@"%f",loc.longitude];
+    
+    NSString latitude = [NSString stringWithFormat:@"%f",loc.latitude];
+     */
+    
+}
+
+
+//Go back to current location
+- (IBAction)backCurrentLocation:(id)sender {
+    
+    MKCoordinateSpan span=MKCoordinateSpanMake(0.01, 0.02);
+    
+    [self.mapView setRegion:MKCoordinateRegionMake(self.mapView.userLocation.coordinate, span) animated:YES];
+}
+
+//called when region changed
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
+{
+    NSLog(@"latitudeDelta:%f,longitudeDelta:%f",mapView.region.span.latitudeDelta,mapView.region.span.longitudeDelta);
+}
+
+
+- (IBAction)minMapView:(id)sender {
+    
+    CGFloat latitudeDelta = self.mapView.region.span.latitudeDelta * 2;
+
+    CGFloat longitudeDelta = self.mapView.region.span.longitudeDelta * 2;
+
+    MKCoordinateSpan span = MKCoordinateSpanMake(latitudeDelta, longitudeDelta);
+
+    MKCoordinateRegion region = MKCoordinateRegionMake(self.mapView.centerCoordinate, span);
+    
+    [self.mapView setRegion:region animated:YES];
+}
+
+- (IBAction)maxMapView:(id)sender {
+    
+    CGFloat latitudeDelta = self.mapView.region.span.latitudeDelta * 0.5;
+
+    CGFloat longitudeDelta = self.mapView.region.span.longitudeDelta * 0.5;
+
+    MKCoordinateSpan span = MKCoordinateSpanMake(latitudeDelta, longitudeDelta);
+
+    MKCoordinateRegion region = MKCoordinateRegionMake(self.mapView.centerCoordinate, span);
+    
+    [self.mapView setRegion:region animated:YES];
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
